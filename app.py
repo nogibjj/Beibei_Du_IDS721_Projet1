f919e06683a550d8b7ad7ece20b9d8f9abeb76dd
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2023-02-08 07:59:05 +0000

    add

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import numpy as np
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import pickle
+import pandas as pd
 
+df = pd.read_csv("data/avg_wage.csv")
+X = df.iloc[:, 1:-1].values
 
 app = Flask(__name__)
 regressor = pickle.load(open("model.pkl", "rb"))
 
-def guess_2022():
-    test_data = np.array([55366, 58092, 61048, 61132, 61634, 61347, 62263, 63845, 63942,
-        64618, 65303, 66383])
+def guess_2022(test_data=X[0]):
     prediction = regressor.predict(test_data)
     print("It is working!")
     return prediction
@@ -16,16 +17,18 @@ def guess_2022():
 
 @app.route('/')
 def first_line():
-    print("This is the first line of the app.py file")
+    print("This is the first line of the app.py file for 2022")
     return 'This is an app to predict the average wage of a country.'
 
-# write me a function that takes in a regressor and returns a prediction
-@app.route('/predict/<regressor>')
-def avg_wage_pred(regressor):
-    print("It is working!")
-    prediction = guess_2022()
-    return prediction
-
+def predict():
+    data = request.get_json()
+    country_data = data['Country']
+    country_data = pd.DataFrame([country_data], columns=['Country', '2000', '2005', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'])
+    X = country_data.iloc[:, 1:].values
+    with open('model.pkl', 'rb') as f:
+        regressor = pickle.load(f)
+    y_pred = regressor.predict(X)
+    return jsonify({'prediction': str(y_pred[0])})
 
 @app.route("/")
 def index():
