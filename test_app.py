1c7c9bc51568b1f44aebc2846963c0eafe660d40
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2023-02-08 09:11:33 +0000

    change

--- test_app.py
+++ test_app.py
@@ -1,10 +1,12 @@
+from app2 import guess_country, predict
+
 def test_guess_country():
     assert guess_country("China") == "The country is not in the dataset!"
     assert guess_country("United States") == "You are correct!"
     assert guess_country("United Kingdom") == "You are wrong"
 
 def test_predict_country():
-    assert predict_country("China") == "The country is not in the dataset!"
-    assert predict_country("United States") == "The average wage in 2020 is 69262.83706995238 in 2020"
-    assert not predict_country("United Kingdom") == "The average wage in 2020 is 69262.83706995238 in 2020"
+    assert predict("China") == "The country is not in the dataset!"
+    assert predict("United States") == "The average wage in 2020 is 69262.83706995238 in 2020"
+    assert not predict("United Kingdom") == "The average wage in 2020 is 69262.83706995238 in 2020"
 
